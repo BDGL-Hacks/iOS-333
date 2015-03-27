19a37b73fe6d49f307334ad0d4dabae26167ff41
Graham Turk <gturk9@gmail.com>
2015-03-27 01:35:37 -0400

    Added text fields and button to login view; made outlet and action connections to ViewController

--- PartyUp/PartyUp/ViewController.swift
+++ PartyUp/PartyUp/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func register(sender: UIButton) {
+        // get nameField and passwordField and send to Blake and David
+    }
 
 }
 

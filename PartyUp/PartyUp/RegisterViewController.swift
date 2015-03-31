b8f0d43b7dc7f45583f9363fb1203f1bcadf4385
Graham Turk <gturk9@gmail.com>
2015-03-31 14:08:11 -0400

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit 16cfa04fc4abeb1e1c9e929b7d46c5401423accc.

--- PartyUp/PartyUp/RegisterViewController.swift
+++ PartyUp/PartyUp/RegisterViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class RegisterViewController: PartyUpViewController {
     
-    // regModel = new RegisterModel()
+   /*--------------------------------------------*
+    * UI Components
+    *--------------------------------------------*/
 
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
@@ -21,28 +23,23 @@ class RegisterViewController: PartyUpViewController {
     @IBOutlet weak var registerButton: UIButton!
     @IBOutlet weak var backToLoginButton: UIButton!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(false)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     
     /*--------------------------------------------*
      * View response methods
      *--------------------------------------------*/
     
     @IBAction func register(sender: UIButton) {
-        backendRegister()
+        var error: NSString? = backendRegister()
+        
+        // Registration successful: Dismiss Registration view and go to homepage
+        if (error == nil) {
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        
+        // Registration failed: Alert user of the failure
+        else {
+            displayAlert("Registration Failed", message: error!)
+        }
     }
     
     @IBAction func backToLogin() {
@@ -62,120 +59,51 @@ class RegisterViewController: PartyUpViewController {
     * Backend Interfacing methods
     *--------------------------------------------*/
     
-    // Saving Cookies: NSHTTPCookieStorage
-    // Put POST stuff in seperate method
-    // Save some constants
-    // Put alert stuff in seperate method
-    
-    @IBAction func backendRegister()
+    /* Performs user registration on the backend.      *
+     * Returns an error message string if registration *
+     * failed, nil otherwise.                          */
+    func backendRegister() -> NSString?
     {
-        NSLog("Register:\n---------------")
+        NSLog("Attempting to register new user...")
         
         var firstName: NSString = firstNameTextField.text
         var lastName: NSString = lastNameTextField.text
         var email: NSString = emailTextField.text
         var password: NSString = passwordTextField.text
         
-        /* TODO: Put IP into a constant! */
-        // Ubuntu IP = "52.4.3.6"
-        // David's Linode IP = "23.239.14.40:8000"
-        var url: NSURL = NSURL(string: "http://23.239.14.40:8000/users/register/")!
+        var postURL: NSString = "http://\(UBUNTU_SERVER_IP)/users/register/"
+        var postParams: [String: String] = ["email": email, "first_name": firstName, "last_name": lastName, "password": password]
         
-        var post: NSString = "email=\(email)&first_name=\(firstName)&last_name=\(lastName)&password=\(password)"
-        var postData: NSData = post.dataUsingEncoding(NSASCIIStringEncoding)!
-        var postLength: NSString = String(postData.length)
-        NSLog("Post: %@", post)
+        var postData: NSDictionary? = sendPostRequest(postParams, url: postURL)
         
-        var request: NSMutableURLRequest = NSMutableURLRequest(URL: url)
-        request.HTTPMethod = "POST"
-        request.HTTPBody = postData
-        request.setValue(postLength, forHTTPHeaderField: "Content-Length")
-        
-        // ?? What's happening here ??
-        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        request.setValue("application/json", forHTTPHeaderField: "Accept")
-        
-        var response: NSURLResponse?
-        var responseError: NSError?
-        
-        var urlData: NSData? = NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: &responseError)
-        
-        if (urlData != nil)
+        // We received JSON data back: process it
+        if (postData != nil)
         {
-            let httpResponse = response as NSHTTPURLResponse
-            NSLog("Response Status Code: %ld", httpResponse.statusCode)
+            let jsonData: NSDictionary = postData!
+            let accepted: Bool = jsonData.valueForKey("accepted") as Bool
+            var errorMessage: NSString? = jsonData.valueForKey("error") as NSString?
             
-            if (httpResponse.statusCode >= 200 && httpResponse.statusCode < 300)
-            {
-                var responseData: NSString = NSString(data: urlData!, encoding: NSUTF8StringEncoding)!
-                NSLog("Response Data %@", responseData)
-                
-                var error: NSError?
-                let jsonData: NSDictionary = NSJSONSerialization.JSONObjectWithData(urlData!, options: NSJSONReadingOptions.MutableContainers, error: &error) as NSDictionary
-                
-                let accepted: Bool = jsonData.valueForKey("accepted") as Bool
-                var errorMessage: NSString? = jsonData.valueForKey("error") as NSString?
-                
-                if (accepted) {
-                    NSLog("Register Successful!")
-                    self.dismissViewControllerAnimated(true, completion: nil)
-                }
-                else
-                {
-                    if (errorMessage != nil) {
-                        NSLog("Register Failed: %@", errorMessage!)
-                    }
-                    else {
-                        NSLog("Register Failed: No error message sent back", errorMessage!)
-                    }
-                    
-                    var alertView: UIAlertView = UIAlertView()
-                    alertView.title = "Sign Up Failed"
-                    alertView.message = errorMessage
-                    alertView.delegate = self
-                    alertView.addButtonWithTitle("OK")
-                    alertView.show()
-                }
+            // Register successful on server side
+            if (accepted) {
+                NSLog("Register Successful!")
+                return nil
             }
-            else
-            {
-                NSLog("Error: Connection Failed")
-                
-                var alertView: UIAlertView = UIAlertView()
-                alertView.title = "Sign Up Failed"
-                alertView.message = "Connection Error: Bad status code"
-                alertView.delegate = self
-                alertView.addButtonWithTitle("OK")
-                alertView.show()
+            
+            // Register was unsuccessful on server side
+            else {
+                if (errorMessage == nil) {
+                    errorMessage = "No error message received from server"
+                }
+                NSLog("Register Failed: %@", errorMessage!)
+                return errorMessage
             }
         }
-        else
-        {
-            NSLog("Error: Connection Failed - No data received back")
-            
-            if (response != nil) {
-                NSLog("Response:%@", response!)
-            }
-            else {
-                NSLog("Response var is nil")
-            }
-            
-            if (responseError != nil) {
-                NSLog("Response Error:%@", responseError!)
-            }
-            else {
-                NSLog("Response Error var is nil")
-            }
-            
-            var alertView: UIAlertView = UIAlertView()
-            alertView.title = "Sign Up Failed"
-            alertView.message = "Connection Failed"
-            alertView.delegate = self
-            alertView.addButtonWithTitle("OK")
-            alertView.show()
+
+        // We did not receive JSON data back
+        else {
+            NSLog("Register Failed: No JSON data received")
+            return "Failed to connect to server"
         }
-        
-        NSLog("---------------")
     }
 
 }

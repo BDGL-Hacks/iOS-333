16c44a862408df060ba7b7c9f66fe5e52b557495
Lance Goodridge <ldgoodridge95@gmail.com>
2015-04-09 16:42:44 -0400

    Minor code refactoring

--- PartyUp/PartyUp/PartyUpBackend.swift
+++ PartyUp/PartyUp/PartyUpBackend.swift
@@ -29,71 +29,10 @@ class PartyUpBackend {
     let UBUNTU_SERVER_IP: NSString = "52.4.3.6"
     let DAVID_LINODE_SERVER_IP: NSString = "23.239.14.40:8000"
     
-    /* title, public?, age_restrictions, price, invite-list */
     
-    /*--------------------------------------------*
+   /*--------------------------------------------*
     * Backend Interfacing Methods
     *--------------------------------------------*/
-    
-    /* Performs event creation      *
-    * Returns an error message string if registration *
-    * failed, nil otherwise.                          */
-    func backendEventCreation(title: NSString, location: NSString, ageRestrictions: NSString,
-        isPublic: NSString, price: NSString, inviteList: [NSString]) -> NSString?
-    {
-        PULog("Attempting to create an event...")
-        
-        var userDefaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
-        let username: NSString = userDefaults.objectForKey("USERNAME") as NSString
-        
-        var postURL: NSString = "http://\(UBUNTU_SERVER_IP)/users/events/create"
-        var postParams: [String: String] = ["title": title, "public": isPublic, "age_restrictions": ageRestrictions, "price": price]
-        
-        var stringOfFriends: String = ""
-        var i: Int = 0
-        for friend in inviteList {
-            if i == 0 {
-                stringOfFriends += friend
-            }
-            else {
-                stringOfFriends += "," + friend
-            }
-            i += 1
-        }
-        postParams["invite_list"] = stringOfFriends
-        
-        var postData: NSDictionary? = sendPostRequest(postParams, url: postURL)
-        
-        // We received JSON data back: process it
-        if (postData != nil)
-        {
-            let jsonData: NSDictionary = postData!
-            let accepted: Bool = jsonData.valueForKey("accepted") as Bool
-            var errorMessage: NSString? = jsonData.valueForKey("error") as NSString?
-            
-            // Event creation successful on server side
-            if (accepted) {
-                PULog("Event Creation Successful!")
-                return nil
-            }
-                
-                // Register was unsuccessful on server side
-            else {
-                if (errorMessage == nil) {
-                    errorMessage = "No error message received from server"
-                }
-                PULog("Event creation Failed: \(errorMessage!)")
-                return errorMessage
-            }
-        }
-            
-            // We did not receive JSON data back
-        else {
-            PULog("Event Creation Failed: No JSON data received")
-            return "Failed to connect to server"
-        }
-    }
-    
     
     /* Performs user authentication on the backend.                    *
      * Returns an error message string if login failed, nil otherwise. */
@@ -179,6 +118,65 @@ class PartyUpBackend {
         }
     }
     
+    /* Performs backend event creation.      *
+     * Returns an error message string if    *
+     * event creation failed, nil otherwise. */
+    func backendEventCreation(title: NSString, location: NSString, ageRestrictions: NSString,
+        isPublic: NSString, price: NSString, inviteList: [NSString]) -> NSString?
+    {
+        PULog("Attempting to create an event...")
+        
+        var userDefaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        let username: NSString = userDefaults.objectForKey("USERNAME") as NSString
+        
+        var postURL: NSString = "http://\(UBUNTU_SERVER_IP)/users/events/create"
+        var postParams: [String: String] = ["title": title, "public": isPublic, "age_restrictions": ageRestrictions, "price": price]
+        
+        var stringOfFriends: String = ""
+        var i: Int = 0
+        for friend in inviteList {
+            if i == 0 {
+                stringOfFriends += friend
+            }
+            else {
+                stringOfFriends += "," + friend
+            }
+            i += 1
+        }
+        postParams["invite_list"] = stringOfFriends
+        
+        var postData: NSDictionary? = sendPostRequest(postParams, url: postURL)
+        
+        // We received JSON data back: process it
+        if (postData != nil)
+        {
+            let jsonData: NSDictionary = postData!
+            let accepted: Bool = jsonData.valueForKey("accepted") as Bool
+            var errorMessage: NSString? = jsonData.valueForKey("error") as NSString?
+            
+            // Event creation successful on server side
+            if (accepted) {
+                PULog("Event Creation Successful!")
+                return nil
+            }
+                
+            // Register was unsuccessful on server side
+            else {
+                if (errorMessage == nil) {
+                    errorMessage = "No error message received from server"
+                }
+                PULog("Event creation Failed: \(errorMessage!)")
+                return errorMessage
+            }
+        }
+            
+        // We did not receive JSON data back
+        else {
+            PULog("Event Creation Failed: No JSON data received")
+            return "Failed to connect to server"
+        }
+    }
+    
     /* Queries backend for events the current user owns,     *
      * is attending, or has been invited to. Returns a       *
      * tuple: an error message string if something went      *
@@ -233,7 +231,7 @@ class PartyUpBackend {
     
     /* Queries backend for a general search of nearby events. *
      * Returns a tuple: an error message string if something  *
-     * went wrong, or the JSON data as NSArray if it was      *
+     * went wrong, and the JSON data as NSArray if it was     *
      * successful.                                            */
     func queryFindEvents(title: NSString? = nil, description: NSString? = nil, isPublic: Bool? = nil,
                           date: NSString? = nil,        time: NSString? = nil,      age: NSInteger? = nil,
@@ -280,7 +278,7 @@ class PartyUpBackend {
             // Query successful: return JSON data as dictionary
             if (accepted) {
                 PULog("Query Successful!")
-                PULog("Query data: \(results)")
+                PULog("Query data: \(results!)")
                 return (nil, results!)
             }
                 

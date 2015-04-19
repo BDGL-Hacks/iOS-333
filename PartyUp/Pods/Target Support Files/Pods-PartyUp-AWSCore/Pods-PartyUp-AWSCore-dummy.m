d60c75a9637ff3b02800b2ffaff8277078cf43c1
Lance Goodridge <ldgoodridge95@gmail.com>
2015-04-19 16:37:47 -0400

    Added AWS iOS SDK v2 Pod

---
+++ PartyUp/Pods/Target Support Files/Pods-PartyUp-AWSCore/Pods-PartyUp-AWSCore-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PartyUp_AWSCore : NSObject
+@end
+@implementation PodsDummy_Pods_PartyUp_AWSCore
+@end
---
+++ PartyUp/Pods/Target Support Files/Pods-PartyUp-AWSSES/Pods-PartyUp-AWSSES-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PartyUp_AWSSES : NSObject
+@end
+@implementation PodsDummy_Pods_PartyUp_AWSSES
+@end

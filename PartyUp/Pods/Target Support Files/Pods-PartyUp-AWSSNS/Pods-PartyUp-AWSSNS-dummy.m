8c299b9b755860559fb3ccb3c1bbe3cac2f83043
Lance Goodridge <ldgoodridge95@gmail.com>
2015-04-28 19:40:01 -0400

    Added Pusher Pod and deleted AWS Pod.

--- PartyUp/Pods/Target Support Files/Pods-PartyUp-AWSSNS/Pods-PartyUp-AWSSNS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PartyUp_AWSSNS : NSObject
-@end
-@implementation PodsDummy_Pods_PartyUp_AWSSNS
-@end
--- PartyUp/Pods/Target Support Files/Pods-PartyUp-AWSSQS/Pods-PartyUp-AWSSQS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PartyUp_AWSSQS : NSObject
-@end
-@implementation PodsDummy_Pods_PartyUp_AWSSQS
-@end
